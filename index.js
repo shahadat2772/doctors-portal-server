28a7467ce14c61604839abd220bc767e405abff7
shahadat2772 <shahadat2772@gmailcom>
2022-05-14 21:54:52 +0600

    get the available slots for specific date menually

--- index.js
+++ index.js
@@ -37,6 +37,97 @@ async function run() {
       res.send(services);
     });
 
+    // app.get("/available", async (req, res) => {
+    //   const date = req.query.date || "May 14, 2022";
+
+    //   // Step 1: Get all services.
+    //   const services = await serviceCollection.find({}).toArray();
+
+    //   // Step 2: Get the bookings of that day.
+
+    //   const query = { date };
+
+    //   const bookingsOnSelectedDay = await bookedAppointmentCollection
+    //     .find(query)
+    //     .toArray();
+
+    //   // Step 3: For each service, find bookings for that service.
+
+    //   services.forEach((service) => {
+    //     const serviceBookings = bookingsOnSelectedDay.filter(
+    //       (b) => b.treatmentName === service.treatmentName
+    //     );
+    //     // const booked = serviceBookings.map((s) => s.slot);
+    //     // service.booked = booked
+
+    //     service.booked = serviceBookings.map((s) => s.slot);
+    //   });
+
+    //   res.send(serviceBookings);
+    // });
+
+    // app.get("/available", async (req, res) => {
+    //   const date = req.query.date || "May 14, 2022";
+
+    //   // Step 1: Get all services
+    //   const services = await serviceCollection.find({}).toArray();
+
+    //   // Step 2: Get all the bookings of that day.
+    //   const bookings = await bookedAppointmentCollection
+    //     .find({ date })
+    //     .toArray();
+
+    //   // Step 3: For each service, find bookings for that service
+    //   services.forEach((service) => {
+    //     const serviceBookings = bookings.filter(
+    //       (b) => b.treatmentName === service.treatmentName
+    //     );
+
+    //     // const bookedSlots = serviceBookings.map((s) => s.slot);
+    //     // services.bookedSlots = bookedSlots;
+
+    //     const bookedSlots = serviceBookings.map((s) => s.slot);
+    //     // const allSlots = service.slots;
+
+    //     const availableSlots = service.slots.filter(
+    //       (slot) => !bookedSlots.includes(slot)
+    //     );
+
+    //     service.availableSlots = availableSlots;
+    //   });
+    //   res.send(services);
+    // });
+
+    app.get("/available", async (req, res) => {
+      const date = req.query.date || "May 14, 2022";
+
+      // Step-1: GET all services
+      const services = await serviceCollection.find({}).toArray();
+
+      // Step-2: Get all booked services
+      const bookedServices = await bookedAppointmentCollection
+        .find({ date })
+        .toArray();
+
+      // Get all bookings for each service
+
+      services.forEach((service) => {
+        const bookingsForThisService = bookedServices.filter(
+          (b) => b.treatmentName === service.treatmentName
+        );
+
+        const bookedSlots = bookingsForThisService.map((s) => s.slot);
+
+        const availableSlots = service.slots.filter(
+          (slot) => !bookedSlots.includes(slot)
+        );
+
+        service.availableSlots = availableSlots;
+      });
+
+      res.send(services);
+    });
+
     /**
      * API Naming Convention
      * app.get('/booking')// get all bookings in this collection or get more then one or by filter
